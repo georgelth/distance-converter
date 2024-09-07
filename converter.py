d1f72bba69da3a6de6ce8bb9e9e1668c24504f72
George Howard <110793196+cpugz@users.noreply.github.com>
2024-09-07 17:17:50 -0500

    added a title

--- converter.py
+++ converter.py
@@ -6,5 +6,9 @@ window = tk.Tk()
 window.title("converter")
 window.geometry("300x150")
 
+# title
+title_label = tk.Label(master = window, text = "miles to kilometers", font = "Calibri 24 bold")
+title_label.pack()
+
 # run
 window.mainloop()
