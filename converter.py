8033f005b661f8431f87669bcf95e35f4391a4fe
George Howard <110793196+cpugz@users.noreply.github.com>
2024-09-07 17:20:25 -0500

    added a entry box and a button

--- converter.py
+++ converter.py
@@ -10,5 +10,13 @@ window.geometry("300x150")
 title_label = tk.Label(master = window, text = "miles to kilometers", font = "Calibri 24 bold")
 title_label.pack()
 
+# input field
+input_frame = ttk.Frame(master = window)
+entry = ttk.Entry(master = input_frame)
+button = ttk.Button(master = input_frame, text = "convert")
+entry.pack()
+button.pack()
+input_frame.pack()
+
 # run
 window.mainloop()
