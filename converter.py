df0dd6165a255d6bab922e7102b8b6549a18e70d
George Howard <110793196+cpugz@users.noreply.github.com>
2024-09-07 18:26:40 -0500

    adding a transparent arrow to reverse conversion

--- converter.py
+++ converter.py
@@ -1,17 +1,21 @@
 import tkinter as tk
 #from tkinter import ttk
 import ttkbootstrap as ttk
+from PIL import Image, ImageTk
 
 # window
-window = ttk.Window(themename = "journal")
+window = ttk.Window(themename = "darkly")
 window.title("converter")
-window.geometry("300x150")
+window.geometry("400x200")
 
-# convert
+# functions
 def convert():
     mile_input = entry_int.get()
     km_output = mile_input * 1.61
     output_string.set(km_output)
+
+def switch_conversion():
+    print("switch conversion")
 
 # title
 title_label = tk.Label(master = window, text = "miles to kilometers", font = "Calibri 24 bold")
@@ -19,10 +23,18 @@ title_label.pack()
 
 # input field
 input_frame = ttk.Frame(master = window)
+
 entry_int = tk.IntVar()
 entry = ttk.Entry(master = input_frame, textvariable = entry_int)
+
 button = ttk.Button(master = input_frame, text = "convert", command = convert)
+
+image = Image.open("assets/conversion.png")
+photo = ImageTk.PhotoImage(image)
+image_button = ttk.Button(master = input_frame, image = photo, command = switch_conversion)
+
 entry.pack(side = "left", padx = 10)
+image_button.pack(side = "left")
 button.pack(side = "left")
 input_frame.pack(pady = 10)
 
